33078b9bbccd9c88a91fdea7fa3fdd8e1a8d3e4a
llx-mbp14 <lilongxiang2000@gmail.com>
2023-04-27 20:21:56 +0800

    fix: switching logic

--- src/hooks/dark.ts
+++ src/hooks/dark.ts
@@ -2,16 +2,22 @@ import { useState } from "react"
 
 
 export default () => {
-  const [isDark, setDark] = useState(false)
+  const _el = document.querySelector('html')
+  const [isDark, setDark] = useState([...(_el?.classList || [])].includes('dark'))
 
   function toogleDark()  {
-    setDark(!isDark)
+    setDark(() => {
+      if (_el) {
+        if (isDark) _el.classList.remove('dark')
+        else _el.classList.add('dark')
+      } else {
+        const __el = document.querySelector('html')
+        if (isDark) __el?.classList.remove('dark')
+        else __el?.classList.add('dark')
+      }
 
-    const el = document.querySelectorAll('html')[0]
-    if (el) {
-      if (isDark) el.classList.add('dark')
-      else el.classList.remove('dark')
-    }
+      return !isDark
+    })
   }
 
   return { isDark, toogleDark }
