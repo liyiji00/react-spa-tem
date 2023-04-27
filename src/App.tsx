196fa1d0dd8ab416a5a19852c384d2c87abeeb57
llx-mbp14 <lilongxiang2000@gmail.com>
2023-04-27 21:38:40 +0800

    feat: main layout

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,15 @@ export default () => {
   return (
     <>
       <Header />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='font-sans text-left text-gray-700 dark:text-gray-200'>
+        <div className='text-center'>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://reactjs.org" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
 
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex suscipit dignissimos possimus asperiores ullam mollitia. Aliquam aliquid neque iste saepe ea, architecto amet, esse iusto ducimus fugiat illum eos voluptates?</p>
       </div>
