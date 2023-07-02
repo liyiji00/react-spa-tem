4eb08272141b07979c0d4f4c1e054061ad1f51dc
longxiang.li <lilongxiang2000@gmail.com>
2023-07-02 21:48:43 +0800

    feat: mediaQuery

--- src/hooks/dark.ts
+++ src/hooks/dark.ts
@@ -1,21 +1,46 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
-export default () => {
+export function useDark() {
+  const [isDark, setDark] = useState(false)
   const _HTML = document.children[0]
-  const [isDark, setDark] = useState(localStorage.getItem('theme') === 'dark')
-  if (isDark) {
-    _HTML.classList.add('dark')
-  }
+
+  useEffect(() => {
+    if (isDark) {
+      _HTML.classList.add('dark')
+      localStorage.setItem('theme', 'dark')
+    } else {
+      _HTML.classList.remove('dark')
+      localStorage.removeItem('theme')
+    }
+  }, [isDark])
+
+  useEffect(() => {
+    const _localTheme = localStorage.getItem('theme')
+    const _mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+
+    const handleChange = (e: MediaQueryListEvent) => {
+      setDark(e.matches)
+    }
+    _mediaQuery.addEventListener('change', handleChange)
+
+    if (_mediaQuery.matches) {
+      if (_localTheme === null) {
+        setDark(true)
+      } else {
+        setDark(_localTheme === 'light' ? false : true)
+      }
+    } else {
+      setDark(_localTheme === 'dark')
+    }
+
+    return () => {
+      _mediaQuery.removeEventListener('change', handleChange)
+    }
+  }, [])
 
   function toggleDark() {
-    setDark(() => {
-      if (isDark) _HTML.classList.remove('dark')
-      else _HTML.classList.add('dark')
-
-      localStorage.setItem('theme', isDark ? '' : 'dark')
-      return !isDark
-    })
+    setDark(!isDark)
   }
 
   return { isDark, toggleDark }
